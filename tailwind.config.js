e04979566b3dff2fc10ddd3a22586c2e6970d815
Bettina Steinbrecher <bettistein@users.noreply.github.com>
2020-03-04 09:49:02 +0100

    startpage details

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,16 +4,25 @@ module.exports = {
       colors: {
         night: {
           default: "#11112F",
-          light: "#161638",
+          light: "#161638"
         },
-        camel: {default: "#F67242", light: "#FFA180"},
-        stone: "#6A6B6F",
-
+        camel: {
+          default: "#F67242",
+          light: "#FFA180"
+        },
+        stone: "#6A6B6F"
       },
+
       minWidth: {
         mobile: "20rem"
+      },
+
+      minHeight: {
+        "3/4": "75%",
+        "24": "24rem"
       }
     },
+
     /* Most of the time we customize the font-sizes,
      so we added the Tailwind default values here for
      convenience */
@@ -23,21 +32,48 @@ module.exports = {
       base: "1rem",
       lg: "1.125rem",
       xl: "1.25rem",
-      '2xl': "1.5rem",
-      '3xl': "1.875rem",
-      '4xl': "2.25rem",
-      '5xl': "3rem",
-      '6xl': "4rem"
+      "2xl": "1.5rem",
+      "3xl": "1.875rem",
+      "4xl": "2.25rem",
+      "5xl": "3rem",
+      "6xl": "4rem"
     },
+
     /* We override the default font-families with our own default prefs  */
     fontFamily: {
-      'sans':['Lato','-apple-system', 'BlinkMacSystemFont', 'Helvetica Neue', 'Arial', 'sans-serif'],
-      'serif': ['Zilla Slab','Georgia', '-apple-system', 'BlinkMacSystemFont', 'Helvetica Neue', 'Arial', 'sans-serif'], 
-      'mono': ['Menlo', 'Monaco', 'Consolas', 'Roboto Mono', 'SFMono-Regular', 'Segoe UI', 'Courier', 'monospace']
-    },
+      sans: [
+        "Lato",
+        "-apple-system",
+        "BlinkMacSystemFont",
+        "Helvetica Neue",
+        "Arial",
+        "sans-serif"
+      ],
+      serif: [
+        "Zilla Slab",
+        "Georgia",
+        "-apple-system",
+        "BlinkMacSystemFont",
+        "Helvetica Neue",
+        "Arial",
+        "sans-serif"
+      ],
+      mono: [
+        "Menlo",
+        "Monaco",
+        "Consolas",
+        "Roboto Mono",
+        "SFMono-Regular",
+        "Segoe UI",
+        "Courier",
+        "monospace"
+      ]
+    }
   },
+
   variants: {
-    width: ['responsive']
+    width: ["responsive"]
   },
+
   plugins: []
-}
+};
