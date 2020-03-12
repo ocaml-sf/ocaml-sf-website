1c43062f7654d2582151c3ac50a38ba77c3b237f
Patrick Stapfer <ryyppy@users.noreply.github.com>
2020-03-12 20:00:28 +0100

    Fix purgecss issues

--- postcss.config.js
+++ postcss.config.js
@@ -1,9 +1,10 @@
 const purgecss = require('@fullhuman/postcss-purgecss')({
   // Specify the paths to all of the template files in your project
   content: [
-    'src/**/*.html',
+    './src/**/*.html',
     './src/**/*.md',
     './src/**/*.liquid',
+    '.eleventy.js',
   ],
 
   whitelist: ["html", "body"],
