2da23227c3b3d83566d60d6a66ec64a26f30b173
Gabriel Scherer <gabriel.scherer@gmail.com>
2026-02-11 16:31:35 +0100

    sponsors: -tezos, -simcorp, Bloomberg becomes Gold

--- tailwind.config.js
+++ tailwind.config.js
@@ -19,6 +19,9 @@ module.exports = {
         platinum: {
           default: "#BFC6CE"
         },
+        gold: {
+          default: "#FFD700"
+        },
         bronze: {
           default: "#D66942"
         }
